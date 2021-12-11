449a19fd4a4a2bed9df4961245f842b3170b733a
payanotty <payanotty@gmail.com>
2021-12-11 14:44:12 +0900

    postできた

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,19 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    username: str
+
 
 app = FastAPI()
 
-@app.get("/hello")
+@app.get("/api/hello")
 def hello():
     return {"hello hello world"}
+
+@app.post("/api/predict")
+async def predict(item: Item):
+    print(item)
+    return item
